5270be9f925162593e339b6e57c546ef9c18d62a
ian grayson <ian133@gmail.com>
2016-02-14 17:03:37 -0800

    igrayson fork

--- remote_conf.js
+++ remote_conf.js
@@ -1,4 +1,4 @@
 module.exports = {
     hub_ip : '',
-    app_id : 'amzn1.echo-sdk-ams.app.999999-d0ed-9999-ad00-999999d00ebe'
+    app_id : 'amzn1.echo-sdk-ams.app.9d50e8c9-e5b0-48e3-870e-4d18482fc3d6',
 };
--- server.js
+++ server.js
@@ -5,22 +5,34 @@ var express = require('express');
 var alexa = require('alexa-app');
 var bodyParser = require('body-parser');
 var https = require('https');
-var privateKey = fs.readFileSync('./cert/private-key.pem', 'utf8');
-var certificate = fs.readFileSync('./cert/certificate.pem', 'utf8');
-var credentials = {key: privateKey, cert: certificate,
-                   //requestCert: true, rejectUnauthorized: true
-                   requestCert: true
-                   };
+var LEX = require('letsencrypt-express');
+var winston = require('winston'),
+    expressWinston = require('express-winston');
 
 // this code is largely taken from
 // https://github.com/matt-kruse/alexa-app-server
 
 // Start up the server
 var expressApp = express();
-var httpsServer = https.createServer(credentials, expressApp);
-var PORT = 443;
 expressApp.use(bodyParser.urlencoded({ extended: true }));
 expressApp.use(bodyParser.json());
+
+expressWinston.requestWhitelist.push('body');
+expressWinston.responseWhitelist.push('body');
+expressApp.use(expressWinston.logger({
+      transports: [
+        new winston.transports.Console({
+          json: true,
+          colorize: true
+        })
+      ],
+      meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+      msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+      expressFormat: true, // Use the default Express/morgan request formatting, with the same colors. Enabling this will override any msg and colorStatus if true. Will only output colors on transports with colorize set to true
+      colorStatus: true, // Color the status code, using the Express/morgan color palette (default green, 3XX cyan, 4XX yellow, 5XX red). Will not be recognized if expressFormat is true
+      ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+    }));
+
 expressApp.set('view engine', 'ejs');
 
 // Register Alexa apps
@@ -28,7 +40,6 @@ expressApp.set('view engine', 'ejs');
 var app = require('./remote.js');
 var endpoint = '/remote';
 expressApp.post(endpoint,function(req,res) {
-    var subject = req.connection.getPeerCertificate().subject;
     app.request(req.body).then(function(response) {
         res.json(response);
     },function(response) {
@@ -37,8 +48,34 @@ expressApp.post(endpoint,function(req,res) {
 });
 // Configure GET requests to run a debugger UI
 expressApp.get(endpoint,function(req,res) {
-    var subject = req.connection.getPeerCertificate().subject;
     res.render('test',{"app":app,"schema":app.schema(),"utterances":app.utterances(),"intents":app.intents});
+});
+
+LEX.create({
+  debug: true,
+  configDir: './letsencrypt.config'                 // ~/letsencrypt, /etc/letsencrypt, whatever you want
+
+, onRequest: expressApp                                    // your express app (or plain node http app)
+
+, letsencrypt: null                                 // you can provide you own instance of letsencrypt
+                                                    // if you need to configure it (with an agreeToTerms
+                                                    // callback, for example)
+
+, approveRegistration: function (hostname, cb) {    // PRODUCTION MODE needs this function, but only if you want
+                                                    // automatic registration (usually not necessary)
+                                                    // renewals for registered domains will still be automatic
+    if (hostname !== 'goetter.duckdns.org') {
+      console.log('Bad domain', hostname);
+      cb(null, null);
+    }
+    cb(null, {
+      domains: [hostname]
+    , email: 'ian133@gmail.com'
+    , agreeTos: true              // you
+    });
+  }
+}).listen([80], [443], function () {
+  console.log("ENCRYPT __ALL__ THE DOMAINS!");
 });
 
 
@@ -47,5 +84,5 @@ expressApp.get(endpoint,function(req,res) {
 //expressApp.use(express.static('public_html'));
 
 //expressApp.listen(PORT);
-httpsServer.listen(PORT);
-console.log("Listening on port "+PORT);
+//httpsServer.listen(PORT);
+//console.log("Listening.);
